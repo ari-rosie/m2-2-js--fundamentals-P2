9f103dd8c37539570c6f16c8ab86430feec3c486
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-16 18:43:22 -0400

    Exercice-12

--- workshop/exercise-12.js
+++ workshop/exercise-12.js
@@ -13,3 +13,24 @@
 // Call the function with [48, 95, 65, 48, 59, 78, 72, 65]
 
 // Try with other values as well
+
+function calculateLetter(grades) {
+    
+    return grades.map(function(grade) {
+        if (grade >= 90)
+            return 'A';
+        else if (grade >= 80)
+            return 'B';
+        else if (grade >= 70)
+            return 'C';
+        else if (grade >= 60)
+            return 'D';
+        else
+            return 'F';
+
+    });
+
+}
+
+const grades = [48, 95, 65, 48, 59, 78, 72, 65];
+console.log(calculateLetter(grades));
